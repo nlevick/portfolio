1e8d823bf08bff810973906d7e1206f3c1f0ae81
leviathan1988@gmail.com <leviathan1988@gmail.com>
2026-02-03 14:29:34 -0700

    [#15] Add content, Card components

--- src/app/(content)/about/page.tsx
+++ src/app/(content)/about/page.tsx
@@ -1,23 +1,47 @@
-import Card from '@/components/Card';
+import Card from '@/components/Card/Card';
+import Subsection from '@/components/Card/Subsection';
 
 export default function About() {
   return (
     <section className="flex-1 overflow-y-scroll overflow-x-hidden">
-      <div className="">
+      <div>
         <Card title="Bio">
-          <p>I dive into complex data to create elegant surfaces.</p>
-          <p>Currenlty building FDA-cleared medical imaging software.</p>
-          <p>Always exploring new depths.</p>
+          <p className="pb-2">Hello, I am Nathan.</p>
+          <p>
+            When I am not at my computer you might find me biking or eating
+            pizza or maybe biking to eat pizza. I live in Albuquerque with my
+            family.
+          </p>
         </Card>
         <Card title="Process">
-          <p>I dive into complex data to create elegant surfaces.</p>
-          <p>Currenlty building FDA-cleared medical imaging software.</p>
-          <p>Always exploring new depths.</p>
-        </Card>
-        <Card title="Thoughts">
-          <p>I dive into complex data to create elegant surfaces.</p>
-          <p>Currenlty building FDA-cleared medical imaging software.</p>
-          <p>Always exploring new depths.</p>
+          <p>
+            I build web apps by spanning the entire UX stack, from user research
+            to production deployment. Using my unconventional journey to
+            software (studying art, math, and screenwriting), my solutions blend
+            creativity, technicality, and storytelling.
+          </p>
+          <div className="relative mt-6 h-px w-full ">
+            <div className="absolute inset-0 bg-gradient-to-r from-transparent via-slate-400/40 to-transparent" />
+          </div>
+          <Subsection title="Research">
+            <p>
+              I start with by understanding users through interviews, usability
+              testing, and analysis.
+            </p>
+          </Subsection>
+
+          <Subsection title="Design">
+            <p>
+              I create lo-fi wireframes explore concepts then usually move
+              directly to code rapid iteration.
+            </p>
+          </Subsection>
+
+          <Subsection title="Code">
+            <p>
+              I build emphasizing scalability, performance, and testability.
+            </p>
+          </Subsection>
         </Card>
       </div>
     </section>
--- src/app/(content)/work/page.tsx
+++ src/app/(content)/work/page.tsx
@@ -1,3 +1,36 @@
+import Card from '@/components/Card/Card';
+import Subsection from '@/components/Card/Subsection';
+
 export default function Work() {
-  return <section className="">work page</section>;
+  return (
+    <section className="flex-1 overflow-y-scroll overflow-x-hidden">
+      <div>
+        <Card title="Projects">
+          <p className="mb-3">
+            I am leading the UI architecture of FDA-cleared digital pathology
+            software at Indica Labs. Check it out{' '}
+            <a
+              className="bold underline"
+              href="https://indicalab.com/clinical-products/halo-ap-dx"
+              target="_blank"
+            >
+              here
+            </a>
+            .
+          </p>
+          <p>
+            More project stories comming soon. Want more details?{' '}
+            <a
+              className="bold underline"
+              href="https://linkedin.com/in/nathan-levick"
+              target="_blank"
+            >
+              Contact me
+            </a>
+            .
+          </p>
+        </Card>
+      </div>
+    </section>
+  );
 }
